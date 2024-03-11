7aa1b1654a5d07c74c835e8f32e46a39d8e9ea25
AdishtJaglan <adisht7524@gmail.com>
2024-03-11 11:49:58 +0530

    add function to generate random color

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const container = document.querySelector(".container");
 const grids = document.querySelectorAll(".grid");
 
+const createRandomColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r},${g},${b})`;
+};
+
 for (let i = 0; i < 16 * 16; i++) {
     const square = document.createElement("div");
     square.classList.add("grid")
@@ -8,6 +16,6 @@ for (let i = 0; i < 16 * 16; i++) {
     container.appendChild(square);
 
     square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "black";
+        square.style.backgroundColor = createRandomColor();
     });
 }
